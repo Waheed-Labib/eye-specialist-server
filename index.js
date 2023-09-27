3183a21421429f3cd4ebf426b7cc7af4ea4ec617
Waheed-Labib <labibwahid28@gmail.com>
2023-09-27 11:02:08 +0600

    added post method to add service

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.port || 5000;
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Eye Specialist Server runing');
@@ -15,33 +16,41 @@ const services = [
         name: 'Cataract',
         price: 200,
         description: 'A condition affecting the eye that causes clouding of the lens. A gradual progression of vision problem, eventually, if not treated, may result in vision loss. Treatment often requires lab test or imaging./pIn Dr. Bean Eye Consultation Center, Patients receive the most technologically advanced care available in a safe and caring environment. The surgery center is designed specifically for cataract surgery and is accredited for the highest quality care possible./pWhether this is your first consult or you are interested in a second opinion, you can trust your eyes to Dr. Bean.',
-        image: ''
+        image: 'https://i.ibb.co/nQt7mxM/cataract.jpg'
     },
     {
         id: 2,
         name: 'Lens Exchange',
         price: 100,
         description: 'Are glasses and contact lenses affecting your lifestyle? Dr. Bean offers the most advanced laser surgery procedures to improve your vision and say goodbye to glasses./pWhether this is your first consult or you are interested in a second opinion, you can trust your eyes to Dr. Bean.',
-        image: ''
+        image: 'https://i.ibb.co/TvBbwQV/lens.webp'
     },
     {
         id: 3,
         name: 'Lasik',
         price: 150,
         description: 'Improve your vision with LASIK (Laser in Situ Keratomileusis) surgery that corrects nearsightedness, farsightedness and astigmatism with a minor all-laser outpatient procedure. Call Dr. Bean today to schedule your consultation./p Dr. Bean uses the latest femtosecond laser or “bladeless” technology. Your vision is corrected almost immediately following surgery and in most cases, you can be back to work the next day./pEnjoy activities without having to worry about your vision!',
-        image: ''
+        image: 'https://i.ibb.co/6PW8PPM/lasik.jpg'
     },
     {
         id: 4,
         name: 'Glaucoma',
         price: 250,
         description: 'A condition where the eye’s optic nerve, which provides information to the brain, is damaged with or without raised intraocular pressure. If untreated, this will cause gradual vision loss. Glaucoma is the second leading cause of blindness in the United States and the most common among African-Americans./pDr. Bean offers patients compassionate care in a comfortable environment.',
-        image: ''
+        image: 'https://i.ibb.co/4YnsKXQ/glaucoma.jpg'
     }
 ]
 
 app.get('/services', (req, res) => {
     res.send(services)
+})
+
+app.post('/services', (req, res) => {
+    const service = req.body;
+    service.id = services.length + 1;
+    services.push(service);
+    console.log(service);
+    res.send(service);
 })
 
 app.listen(port, () => {
